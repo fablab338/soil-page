3ef432e58f975fd662db150b29a689aa12db981e
アマ <amayu5610@gmail.com>
2026-05-17 03:18:38 +0900

    fix password reset email

--- server.js
+++ server.js
@@ -98,7 +98,9 @@ const otpStore = {};
 // =========================
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
