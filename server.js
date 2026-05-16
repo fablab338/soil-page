6ea4f258d079339b247219438468689f1f4cdb10
アマ <amayu5610@gmail.com>
2026-05-17 02:19:16 +0900

    fix gmail smtp settings

--- server.js
+++ server.js
@@ -98,13 +98,14 @@ const otpStore = {};
 // =========================
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
 });
-
 
 // =========================
 // 新規登録
