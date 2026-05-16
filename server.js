bd0f461b1d03cdd6d6553d8a05fce06de6312c5b
アマ <amayu5610@gmail.com>
2026-05-17 03:12:40 +0900

    fix gmail email config

--- server.js
+++ server.js
@@ -98,9 +98,7 @@ const otpStore = {};
 // =========================
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
